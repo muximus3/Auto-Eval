767d3bb7899a21396582c06f2184697c4f2772db
muximus3 <muximus10@gmail.com>
2023-05-09 15:06:47 +0800

    Change code architecture

---
+++ eval/commands/auto_eval.py
@@ -0,0 +1,78 @@
+import os
+import sys
+import argparse
+from oneapi import OneAPITool
+sys.path.append(os.path.normpath(f"{os.path.dirname(os.path.abspath(__file__))}/../../"))
+from eval import eval_one_group, eval_one_qa, eval_groups, EvalConfig
+from eval.utils.data_utils import df_reader
+from eval import prompt_template
+    
+def add_shared_arguments(parser):
+    parser.add_argument("-c", "--config_file", type=str, help="config file path", required=True)
+    parser.add_argument("-tp", "--template_path", type=str, default=None, help="eval prompt template path", required=False)
+    parser.add_argument("-v", "--verbose", type=bool, default=True, help="print every prompt and response detail", required=False)
+    parser.add_argument("-m", "--model", type=str, default="", help="evaluate model name, e.g., gpt-35-turbo, gpt-4", required=False)
+    parser.add_argument("-te", "--temperature", type=float, default=0.1, help="0-1, higher temperature more random result", required=False)
+    parser.add_argument("-mnt", "--max_new_tokens", type=int, default=2048, help="max output token length", required=False)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="auto-eval <command> [<args>]")
+    subparsers = parser.add_subparsers(dest="command")
+
+    # auto-eval line
+    line_parser = subparsers.add_parser("line", help="auto-eval line [<args>]")
+    add_shared_arguments(line_parser)
+    line_parser.add_argument("-p", "--prompt", type=str, help="question", required=True)
+    line_parser.add_argument("-a", "--answers", nargs='+', help="candidate answers", required=True)
+    line_parser.add_argument("-ta", "--target", type=str, default="", help="standard answer", required=False)
+
+    # auto-eval file
+    file_parser = subparsers.add_parser("file", help="auto-eval file [<args>]")
+    add_shared_arguments(file_parser)
+    file_parser.add_argument("-edp", "--eval_data_path", nargs="+", help="one or more eval data path", required=True)
+    file_parser.add_argument("-op", "--output_path", type=str, default="", help="", required=False)
+    file_parser.add_argument("-ec", "--eval_categories", default=None, nargs="+", help="only evaluate chosen categories", required=False)
+    file_parser.add_argument("-sn", "--sample_num", type=int, default=0, help="", required=False)
+    file_parser.add_argument("-i", "--interval", type=int, default=1, help="request interval, gpt-4 need longer interval, e.g.,10s", required=False)
+    file_parser.add_argument("-r", "--retry", type=bool, default=True, help="", required=False)
+
+    args = parser.parse_args()
+    if not args.template_path:
+        template_path =  os.path.join(os.path.dirname(prompt_template.__file__), 'eval_prompt_template.json')
+    else:
+        template_path = args.template_path
+    eval_prompter = prompt_template.EvalPrompter.from_config(template_path, verbose=args.verbose)
+
+    if args.command == "line":
+        tool = OneAPITool.from_config_file(args.config_file)
+        score, raw_response = eval_one_qa(
+            eval_prompter=eval_prompter,
+            question=args.prompt, 
+            candidate_answers=args.answers, 
+            target=args.target,
+            api_tool=tool,
+            model=args.model,
+            temperature=args.temperature,
+            max_new_tokens=args.max_new_tokens,
+            )
+        print(f'\nSCORE:\n{score}')
+
+    elif args.command == "file":
+        eval_groups(
+            EvalConfig(
+            eval_prompter=eval_prompter,
+            api_config_file=args.config_file, 
+            eval_data_path=args.eval_data_path, 
+            output_path=args.output_path, 
+            model=args.model,
+            eval_categories=args.eval_categories,
+            sample_num=args.sample_num, 
+            request_interval=args.interval,
+            retry=args.retry,
+            temperature=args.temperature,
+            max_new_tokens=args.max_new_tokens,
+            ))
+
+if __name__ == "__main__":
+    main()
---
+++ eval/prompt_template/prompter.py
@@ -0,0 +1,74 @@
+import os
+from typing import List
+import abc
+import json
+from eval.utils import load_json, generate_letters, extract_last_json, extract_scores
+
+class Prompter(abc.ABC):
+    @abc.abstractmethod
+    def generate_prompt(**kwargs):
+        raise NotImplementedError
+    
+    @abc.abstractmethod
+    def extract_result_from_response(response):
+        raise NotImplementedError
+
+class EvalPrompter(Prompter):
+    __slots__ = ( "eval_with_target_template", "eval_without_target_template", "_verbose")
+
+    @classmethod
+    def from_config(cls, prompt_template_file: str, verbose: bool = True):
+        if not os.path.exists(prompt_template_file):
+            raise ValueError(f'File not exists:{prompt_template_file}')
+        template_data = load_json(prompt_template_file)
+        eval_with_target_template = template_data['eval_with_target_template']
+        eval_without_target_template = template_data[ 'eval_without_target_template']
+        if verbose:
+            print(f'Using prompt template:\n {json.dumps(template_data, sort_keys=True, indent=2)}')
+        return cls(eval_with_target_template, eval_without_target_template, verbose)
+
+    def __init__(self,
+                 eval_with_target_template: str,
+                 eval_without_target_template: str,
+                 verbose: bool = True) -> None:
+        self._verbose = verbose
+        self.eval_with_target_template = eval_with_target_template
+        self.eval_without_target_template = eval_without_target_template
+
+    def generate_prompt(self, question: str, target: str,
+                        candidate_answers: List[str]) -> str:
+        """
+        Generate a prompt based on the given question, target, and candidate answers.
+        """
+        candidate_answer_numbers = generate_letters(len(candidate_answers))
+        format_option_data = '\n'.join([
+            f'**{candidate_answer_numbers[i]}**. {candidate_answers[i]}'
+            for i in range(len(candidate_answers))
+        ])
+
+        if target:
+            eval_prompt = self.eval_with_target_template.format(
+                question=question, target=target, answers=format_option_data)
+        else:
+            eval_prompt = self.eval_without_target_template.format(
+                question=question, answers=format_option_data)
+        if self._verbose:
+            print(
+                f"\n{'-'*20} prompt detail 🚀  {'-'*20}\n\n{eval_prompt}\n\n{'-'*20} prompt end {'-'*20}"
+            )
+        return eval_prompt
+
+    def extract_result_from_response(self, response: str) -> List[float]:
+        """
+        Extract the result (scores) from the given model response.
+        """
+        if self._verbose:
+            print(
+                f"{'-'*20} response detail ⭐️ {'-'*20}\n\n{response}\n\n{'-'*20} response end {'-'*20}\n"
+            )
+        try:
+            result_json = extract_last_json(response.strip())
+        except (AttributeError, json.decoder.JSONDecodeError):
+            result_json = extract_scores(response.strip())
+        scores = list(map(lambda x: float(x), result_json.values()))
+        return scores
