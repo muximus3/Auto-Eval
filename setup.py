e19a224b1bbccf8b42bb25fe1aefe4ec9eca6438
muximus3 <muximus10@gmail.com>
2023-05-10 18:54:58 +0800

    sort score result by both category and model

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="auto-eval",
-    version="0.1.8",
+    version="0.1.9",
     packages=find_packages(),
     package_data={
       "eval":["prompt_template/eval_prompt_template.json"]  
